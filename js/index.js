19b92ef8d3528f5ad8f9176066bc37fa17c63633
panddrк <panddr@gmail.com>
2017-05-04 16:29:48 +0300

    1

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
   var homepage = document.getElementById("homepage");
+  var projectHorizontal = document.getElementById("project-horizontal");
   var imagesHomepage = ['1.2.jpg', '2.jpg', '3.jpg'];
 
   if (homepage) {
     var img = document.createElement('img');
     var i = Math.floor(Math.random() * 3);
-    console.log(imagesHomepage[i]);
     img.setAttribute("src",imagesHomepage[i]);
     homepage.appendChild(img);
+  }
+
+  if (projectHorizontal) {
+    var img = document.querySelectorAll('img');
+    var imgWidth = img[0].getBoundingClientRect().width;
+    projectHorizontal.style.width = imgWidth * ( img.lenght() + 1 );
   }
 });
