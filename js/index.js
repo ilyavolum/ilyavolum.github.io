4bc2d84ae1c270eba3d00ffe6f7bc079a9ae2f19
panddr <panddr@gmail.com>
2017-05-04 17:59:27 +0300

    horizontal slider

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   var homepage = document.getElementById("homepage");
   var projectHorizontal = document.getElementById("project-horizontal");
   var imagesHomepage = ['1.2.jpg', '2.jpg', '3.jpg'];
-  var wWidth = window.innerWidth;
 
   if (homepage) {
     var img = document.createElement('img');
@@ -11,21 +10,28 @@ document.addEventListener("DOMContentLoaded", function() {
     homepage.appendChild(img);
   }
 
-  if (projectHorizontal) {
-    var img = document.querySelectorAll('img');
-    if (wWidth > 1100) {
-      var imgWidth = 800;
-    } else if (wWidth > 900) {
-      var imgWidth = 700;
-    } else if (wWidth > 600) {
-      var imgWidth = 400;
-    } else {
-      return;
-    }
+  window.addEventListener("resize", handleResize);
+  handleResize();
 
-    for (var i = img.length - 1; i >= 0; i--) {
-      img[i].style.width = imgWidth + 'px';
+  function handleResize() {
+    var wWidth = window.innerWidth;
+    if (projectHorizontal && wWidth > 600) {
+      var img = document.querySelectorAll('img');
+      var sliderWidth = 0;
+
+      var int = setInterval(function() {
+        if (img[0].getBoundingClientRect().width) {
+          clearInterval(int);
+
+          for (var i = img.length - 1; i >= 0; i--) {
+            var imgWidth = img[i].getBoundingClientRect().width;
+            sliderWidth = sliderWidth + imgWidth + 45;
+          }
+          projectHorizontal.style.width = sliderWidth + 'px';
+        }
+      }, 100);
+    } else {
+      projectHorizontal.style.width = 'auto';
     }
-    projectHorizontal.style.width = imgWidth * ( img.length + 1 ) - 100 + 'px';
   }
 });
