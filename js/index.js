94556d7b60dd0bcb9426f129a195cb2b255d5195
panddr <panddr@gmail.com>
2017-05-04 17:05:49 +0300

    horizontal project

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var homepage = document.getElementById("homepage");
   var projectHorizontal = document.getElementById("project-horizontal");
   var imagesHomepage = ['1.2.jpg', '2.jpg', '3.jpg'];
+  var wWidth = window.innerWidth;
 
   if (homepage) {
     var img = document.createElement('img');
@@ -12,7 +13,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
   if (projectHorizontal) {
     var img = document.querySelectorAll('img');
-    var imgWidth = img[0].getBoundingClientRect().width;
-    projectHorizontal.style.width = imgWidth * ( img.lenght() + 1 );
+    if (wWidth > 1100) {
+      var imgWidth = 800;
+    } else if (wWidth > 900) {
+      var imgWidth = 700;
+    } else if (wWidth > 600) {
+      var imgWidth = 400;
+    } else {
+      return;
+    }
+
+    for (var i = img.length - 1; i >= 0; i--) {
+      img[i].style.width = imgWidth + 'px';
+    }
+    projectHorizontal.style.width = imgWidth * ( img.length + 1 ) - 100 + 'px';
   }
 });
